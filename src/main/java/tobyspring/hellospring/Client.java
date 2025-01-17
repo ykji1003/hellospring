f545ee13c7f3defc224a8fac1bd1e7dcd67a856a
jaeikKim <ykji1003@hotmail.co.kr>
2025-01-17 17:30:12 +0900

    [추가] 스프링 IoC/DI 컨테이너 추가

--- src/main/java/tobyspring/hellospring/Client.java
+++ src/main/java/tobyspring/hellospring/Client.java
@@ -1,12 +1,16 @@
 package tobyspring.hellospring;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import java.io.IOException;
 
 public class Client {
     public static void main(String[] args) throws IOException {
 
-        ObjectFactory objectFactory = new ObjectFactory();
-        PaymentService paymentService = objectFactory.paymentService();
+        BeanFactory beanFactory = new AnnotationConfigApplicationContext(ObjectFactory.class);
+        PaymentService paymentService = beanFactory.getBean(PaymentService.class);
+
 
         Payment payment = paymentService.prepare(100L, "USD", java.math.BigDecimal.valueOf(50.7));
         System.out.println(payment);
